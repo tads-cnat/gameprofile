bb09daa509a1b64f0128e399221de3defcb02bb6
Tonny-Weslley <tonnyweslleyss@gmail.com>
2022-12-08 09:45:18 -0300

    atualizando lista de itens

--- frontend/src/components/sidebar.js
+++ frontend/src/components/sidebar.js
@@ -6,8 +6,9 @@ function Sidebar() {
             <h1>React App</h1>
             <ul>
                 <li>Home</li>
-                <li>About</li>
-                <li>Contact</li>
+                <li>Salas</li>
+                <li>Perfil</li>
+                <li>Ajustes</li>
             </ul>
         </div>
     )
