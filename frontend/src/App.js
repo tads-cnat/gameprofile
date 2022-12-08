d2ee323bed4c5cc495c212acd50249fdbae5c364
Tonny-Weslley <tonnyweslleyss@gmail.com>
2022-12-08 09:17:33 -0300

    iniciando navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
+import React from 'react';
+import Sidebar from './components/sidebar';
+
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <Sidebar />
     </div>
   );
 }
---
+++ frontend/src/components/sidebar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <h1>React App</h1>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Sidebar;
