cab8c256951f705b120ad0cecce458f8ef2d9ecb
Tonny Weslley <70965358+Tonny-Weslley@users.noreply.github.com>
2022-12-11 21:48:23 +0000

    criando componente de lane

---
+++ frontend/src/components/create-group/card-lane.js
@@ -0,0 +1,8 @@
+function CardLane(lane){
+
+
+
+
+}
+
+export default CardLane;
--- frontend/src/components/create-group/create-group.js
+++ frontend/src/components/create-group/create-group.js
@@ -2,7 +2,23 @@ function CreateGroup(){
 
     return(
         <div>
-            <h1>Create Group</h1>
+            <form>
+                <div>
+                    <label for='nome'></label>
+                    <input type="text" id='nome'/>
+                </div>
+                <div>
+                    {/* date input */}
+                    <label for='data'></label>
+                    <input type="date" id='data'/>
+                </div>
+                <div>
+                    {/* time input */}
+                    <label for='hora'></label>
+                    <input type="time" id='hora'/>
+                </div>
+                
+            </form>
         </div>
     )
 
