2977be3c31f12afc607c591f5d8295df2ef05539
Tonny-Weslley <tonnyweslleyss@gmail.com>
2022-12-12 02:05:31 -0300

    atualizano abordagem de modal para pagina

---
+++ frontend/src/pages/create-group.js
@@ -0,0 +1,16 @@
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
+function CreateGroup(){
+
+
+    return(
+        <div>
+            
+        </div>
+    )
+
+}
+
+export default CreateGroup;
