0a9924b195665c679e12859156f18425da20abe7
patrickmf <patrick.work56@gmail.com>
2023-01-25 22:38:31 -0300

    #swagger

---
+++ backend/grupos-partidas-apis/src/main/java/com/gameprofile/grupospartidasapis/configurations/SwaggerConfiguration.java
@@ -0,0 +1,14 @@
+/*
+package com.gameprofile.grupospartidasapis.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfiguration {
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationTypes.SWAGGER_2).select().api()
+    }
+}
+*/
