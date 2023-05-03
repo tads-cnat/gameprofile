6a92b85e164f724aec9e89adac46345424f678e5
Ruan Franklin <94229859+Ruan-Franklin@users.noreply.github.com>
2023-05-03 14:51:25 +0000

    :recycle: Retira métodos de EntradaController

--- backend/grupos-partidas-apis/src/main/java/com/gameprofile/grupospartidasapis/controllers/EntradaController.java
+++ backend/grupos-partidas-apis/src/main/java/com/gameprofile/grupospartidasapis/controllers/EntradaController.java
@@ -2,32 +2,13 @@ package com.gameprofile.grupospartidasapis.controllers;
 
 import com.gameprofile.grupospartidasapis.entities.Entrada;
 import com.gameprofile.grupospartidasapis.repositories.EntradaRepository;
-import java.util.List;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
 @RequestMapping("/entradas")
 public class EntradaController extends CustomController<Entrada, Integer, EntradaRepository> {
     public EntradaController(EntradaRepository repository){
-        super (repository);
-    }
-
-    @GetMapping("/{id}")
-    public Entrada findById(@PathVariable Integer id) {
-        return super.findById(id, getRepository(), null);
-    }
-    @GetMapping
-    public List<Entrada> findAll() {
-        return getRepository().findAll();
-    }
-
-    @PostMapping
-    public Entrada insert(@RequestBody Entrada entrada) {
-        return super.save(entrada, getRepository());
+        super(repository);
     }
 }
