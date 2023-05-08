ebb560098d996be7097e2b34790dc3b455b366b7
Dina Patrícia <dinapatriciag@gmail.com>
2023-05-08 01:36:49 -0300

    configurando o docker no front

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    watch: {
+     usePolling: true,
+    },
+    strictPort: true,
+    port: 3000,
+    host: "0.0.0.0",
+  }
+});
