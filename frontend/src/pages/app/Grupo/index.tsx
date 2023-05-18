f34259ca888ca2802cd80f3d6518ead632904551
Dina Patrícia <dinapatriciag@gmail.com>
2023-05-18 01:18:54 -0300

    criando componente do mapa

--- frontend/src/pages/app/Grupo/index.tsx
+++ frontend/src/pages/app/Grupo/index.tsx
@@ -1,7 +1,10 @@
+import Map from "../../../components/map";
+
 const Grupo = () => {
     return(
         <div className="app-area">
             <h1>Grupo</h1>
+            <Map />
         </div>
     );
 };
