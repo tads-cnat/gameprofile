2ce78333db9ae92e62fa09ca9a11c04415f83b90
Ruan Franklin <94229859+Ruan-Franklin@users.noreply.github.com>
2023-05-21 13:36:11 +0000

    Adiciona client html

--- backend/grupos-partidas-apis/src/main/java/com/gameprofile/grupospartidasapis/controllers/ChatController.java
+++ backend/grupos-partidas-apis/src/main/java/com/gameprofile/grupospartidasapis/controllers/ChatController.java
@@ -2,26 +2,34 @@ package com.gameprofile.grupospartidasapis.controllers;
 
 import com.gameprofile.grupospartidasapis.entities.ChatMessage;
 import com.gameprofile.grupospartidasapis.repositories.ChatRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDateTime;
 
 @Controller
 public class ChatController {
 
     private final ChatRepository repository;
+    private final SimpMessagingTemplate messagingTemplate;
 
-    @Autowired
-    public ChatController(ChatRepository repository) {
-        this.repository = repository;
+    @GetMapping("/conversa")
+    public String conversa(){
+        return "index";
     }
 
-    @MessageMapping("/chat") 
-    @SendTo("/topic/messages") 
-    public ChatMessage handleMessage(@Payload ChatMessage message) {
+    public ChatController(ChatRepository repository, SimpMessagingTemplate messagingTemplate) {
+        this.repository = repository;
+        this.messagingTemplate = messagingTemplate;
+    }
+
+    @MessageMapping("/chat")
+    public void handleMessage(@Payload ChatMessage message) {
+        message.setDataEnvio(LocalDateTime.now());
         ChatMessage savedMessage = repository.save(message);
-        return savedMessage;
+        messagingTemplate.convertAndSend("/topic/messages", savedMessage);
     }
 }
