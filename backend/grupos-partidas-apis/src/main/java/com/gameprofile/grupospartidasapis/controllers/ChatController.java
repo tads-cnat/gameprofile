c27e0e36c5ee7919e2a1a37199b015e81da1c825
Ruan Franklin <94229859+Ruan-Franklin@users.noreply.github.com>
2023-05-21 14:58:25 +0000

    :fire: remove cliente de navegador

--- backend/grupos-partidas-apis/src/main/java/com/gameprofile/grupospartidasapis/controllers/ChatController.java
+++ backend/grupos-partidas-apis/src/main/java/com/gameprofile/grupospartidasapis/controllers/ChatController.java
@@ -6,7 +6,6 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import java.time.LocalDateTime;
 
@@ -15,11 +14,6 @@ public class ChatController {
 
     private final ChatRepository repository;
     private final SimpMessagingTemplate messagingTemplate;
-
-    @GetMapping("/conversa")
-    public String conversa(){
-        return "index";
-    }
 
     public ChatController(ChatRepository repository, SimpMessagingTemplate messagingTemplate) {
         this.repository = repository;
