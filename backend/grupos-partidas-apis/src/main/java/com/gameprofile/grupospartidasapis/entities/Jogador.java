c767cffb4f2ddcd10df00e3e4a042072f9e5527c
Dina Gomes <74661038+dinapgomes@users.noreply.github.com>
2023-06-07 00:24:46 +0000

    :recycle: refactory: refatora entidade jogador

--- backend/grupos-partidas-apis/src/main/java/com/gameprofile/grupospartidasapis/entities/Jogador.java
+++ backend/grupos-partidas-apis/src/main/java/com/gameprofile/grupospartidasapis/entities/Jogador.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @Entity(name= "jogadores")
 @Data
+@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
 public class Jogador{
     @Id
     @Column(name = "id_jogador")
@@ -54,4 +55,7 @@ public class Jogador{
 
     @OneToMany(mappedBy = "jogador")
     private List<ChatMessage > chatMessages;
+
+    @OneToMany(mappedBy = "jogador")    
+    private List<Entrada> entradas;
 }
