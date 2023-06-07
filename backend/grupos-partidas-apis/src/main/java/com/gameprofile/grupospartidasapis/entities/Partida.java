2103ffedc9745d5b04700123b1aceeddb338612e
Lázaro Lawrence Pereira Damasceno <62726118+LazaroDamasceno@users.noreply.github.com>
2023-06-07 16:40:28 +0000

    Remoção de anotações desnecessárias na entidade de Partida

--- backend/grupos-partidas-apis/src/main/java/com/gameprofile/grupospartidasapis/entities/Partida.java
+++ backend/grupos-partidas-apis/src/main/java/com/gameprofile/grupospartidasapis/entities/Partida.java
@@ -13,39 +13,23 @@ import java.util.Date;
 public class Partida implements Identifiable <Integer>{
 
     @Id
-    @Column(name = "id_partida")
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer idPartida;
+    private Integer id;
 
-    @Column(name = "ranqueada")
     private Boolean ranqueada;
 
-    @Column(name = "status")
     @NotNull(message="{NotNull.Partida.status}")
     private String status;
 
     @DateTimeFormat(pattern = "hh:mm")
-    @Column(name = "horario")
     @NotNull(message="{NotNull.Partida.horario}")
     private final LocalTime horario;
 
     @DateTimeFormat(pattern = "dd/mm/yyyy")
-    @Column(name = "data")
     @NotNull(message="{NotNull.Partida.data}")
     private Date data;
 
-    @Column(name = "id_grupo")
     @NotNull(message="{NotNull.Partida.idGrupo}")
     private Integer idGrupo;
-
-    @Override
-    public Integer getId() {
-        return this.idPartida;
-}
-    @Override
-
-    public void setId(Integer id){
-        this.idPartida = id;
-    }
 
 }
