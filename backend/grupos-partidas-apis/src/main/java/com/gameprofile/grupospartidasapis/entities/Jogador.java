ee453388f15f7a839f1b53991f64b8479d6e6d7a
Lázaro Lawrence Pereira Damasceno <62726118+LazaroDamasceno@users.noreply.github.com>
2023-06-07 16:38:27 +0000

    Remoção de anotações desnecessárias na entidade de Jogador

--- backend/grupos-partidas-apis/src/main/java/com/gameprofile/grupospartidasapis/entities/Jogador.java
+++ backend/grupos-partidas-apis/src/main/java/com/gameprofile/grupospartidasapis/entities/Jogador.java
@@ -16,40 +16,31 @@ import java.util.List;
 @Data
 public class Jogador{
     @Id
-    @Column(name = "id_jogador")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer idJogador; 
+    private Integer idJ; 
     
-    @Column(name = "nickname")
     @NotNull(message="{NotNull.Jogador.nickname}")
     private String nickname;
 
-    @Column(name = "nome")
     @NotNull(message="{NotNull.Jogador.nome}")
     private String nome;
 
-    @Column(name = "email")
     @NotNull(message="{NotNull.Jogador.email}")
     private String email;
 
-    @Column(name = "senha")
     @NotNull(message="{NotNull.Jogador.senha}")
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String senha;
 
     @DateTimeFormat(pattern = "dd/mm/yyyy")
-    @Column(name = "nascimento")
     private Date nascimento;
 
     @DateTimeFormat(pattern = "dd/mm/yyyy")
-    @Column(name = "entrou_em")
     private Date entrouEm;
 
     @DateTimeFormat(pattern = "dd/mm/yyyy")
-    @Column(name = "editado_em")
     private Date editadoEm;
 
-    @Column(name = "id_lol")
     private Integer idLol;
 
     @OneToMany(mappedBy = "jogador")
