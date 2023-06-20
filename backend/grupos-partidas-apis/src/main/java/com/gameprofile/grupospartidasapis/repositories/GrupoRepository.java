ff77e88a957f097f45a2f509d0c8be8838069098
Ruan Franklin <94229859+Ruan-Franklin@users.noreply.github.com>
2023-06-20 13:11:51 +0000

    Adiciona Factory de grupo

--- backend/grupos-partidas-apis/src/main/java/com/gameprofile/grupospartidasapis/repositories/GrupoRepository.java
+++ backend/grupos-partidas-apis/src/main/java/com/gameprofile/grupospartidasapis/repositories/GrupoRepository.java
@@ -2,6 +2,9 @@ package com.gameprofile.grupospartidasapis.repositories;
 
 import com.gameprofile.grupospartidasapis.entities.Grupo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface GrupoRepository extends JpaRepository<Grupo, Integer> {
+    Optional<Grupo> findByNome(String nome);
+
 }
