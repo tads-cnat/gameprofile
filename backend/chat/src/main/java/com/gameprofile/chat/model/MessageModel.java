9ea9cb7b783c7cf235d3803bd233d76ff4f0d15e
lazarodamasceno <lazarolawrence2@gmail.com>
2023-06-21 23:44:46 -0300

    Criou a classe de modelo da message e o enum associado a ela

---
+++ backend/chat/src/main/java/com/gameprofile/chat/model/MessageModel.java
@@ -0,0 +1,24 @@
+package com.gameprofile.chat.model;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class MessageModel {
+
+    private String senderName;
+    private String receiverName;
+    private String message;
+    private LocalDateTime dateTime;
+    private Status status;
+    
+}
---
+++ backend/chat/src/main/java/com/gameprofile/chat/model/Status.java
@@ -0,0 +1,7 @@
+package com.gameprofile.chat.model;
+
+public enum Status {
+    JOIN,
+    MESSAGE,
+    LEAVE
+}
