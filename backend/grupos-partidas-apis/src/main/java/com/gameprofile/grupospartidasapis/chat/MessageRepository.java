7384e5dcf0f554feaf42c1a482453977df83ae82
lazarodamasceno <lazarolawrence2@gmail.com>
2023-06-21 21:46:10 -0300

    Deleta a pasta de chat dentro de backend

--- backend/grupos-partidas-apis/src/main/java/com/gameprofile/grupospartidasapis/chat/MessageRepository.java
+++
@@ -1,6 +0,0 @@
-package com.gameprofile.grupospartidasapis.chat;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MessageRepository extends JpaRepository<MessageEntity, Long> {
-}
--- backend/grupos-partidas-apis/src/main/java/com/gameprofile/grupospartidasapis/chat/MessageStatus.java
+++
@@ -1,7 +0,0 @@
-package com.gameprofile.grupospartidasapis.chat;
-
-public enum MessageStatus {
-    JOIN,
-    MESSAGE,
-    LEAVE
-}
