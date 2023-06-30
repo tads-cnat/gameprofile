20ac203cc470f9ad0266029d4e714a43f902597a
Lázaro Lawrence Pereira Damasceno <62726118+LazaroDamasceno@users.noreply.github.com>
2023-06-30 02:34:53 +0000

    Continuação da implementação do chat (ainda incompleto e com alguns erros)

--- backend/grupos-partidas-apis/src/main/resources/templates/frontend-chat/src/chat/ChatRoom.tsx
+++ backend/grupos-partidas-apis/src/main/resources/templates/frontend-chat/src/chat/ChatRoom.tsx
@@ -1,18 +1,42 @@
 import { useState } from "react"
+import { over } from 'stompjs'
+import SockJs from 'sockjs-client'
 
 export default function ChatRoom() {
     
-    const [userData, setUserData] = useState([
+    const [userData, setUserData] = useState({
         username: "",
         receivername: "",
         connected: false,
         message: ""
-    ])
+    })
+
+    const handleUserName = (event) => {
+        const {value} = event.target
+        setUserData({...userData, "username": value})
+    }
+
+    const registerUser = () => {
+
+    }
 
     return (
-        <>
-        
-        </>
+        <div className='container'>
+            {userData.connected?
+            <div></div>
+            :
+            <div className=''>
+                <input 
+                    id='user_name'
+                    placeholder="Informe seu nome de usuário"
+                    value={userData.username}
+                    onChange={handleUserName}
+                />
+                <button type="button" onClick={registerUser}>
+
+                </button>
+            </div>}
+        </div>
     )
 
 }
