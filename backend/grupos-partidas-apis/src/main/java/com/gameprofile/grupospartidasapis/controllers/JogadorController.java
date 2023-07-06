09dabdcdb462d6361eeb6128813140145bb51073
Ruan Franklin <94229859+Ruan-Franklin@users.noreply.github.com>
2023-07-06 00:55:24 +0000

    feat: adiciona service no controller

--- backend/grupos-partidas-apis/src/main/java/com/gameprofile/grupospartidasapis/controllers/JogadorController.java
+++ backend/grupos-partidas-apis/src/main/java/com/gameprofile/grupospartidasapis/controllers/JogadorController.java
@@ -2,10 +2,13 @@ package com.gameprofile.grupospartidasapis.controllers;
 
 import com.gameprofile.grupospartidasapis.entities.Jogador;
 import com.gameprofile.grupospartidasapis.repositories.JogadorRepository;
+import com.gameprofile.grupospartidasapis.services.JogadorService;
 import org.hibernate.ObjectNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import java.util.Collections;
 import java.util.List;
@@ -16,38 +19,40 @@ import java.util.Optional;
 
 public class JogadorController {
         private JogadorRepository repository;
-
+        private JogadorService jogadorService;
         private RestTemplate restTemplate;
-        public JogadorController(JogadorRepository repository, RestTemplate restTemplate){
-            this.repository=repository;
+
+        public JogadorController(JogadorService jogadorService, RestTemplate restTemplate){
+            this.jogadorService=jogadorService;
             this.restTemplate=restTemplate;
         }
 
         @GetMapping
         public List<Jogador> findAll() {
             try {
-                return repository.findAll();
-            } catch(IllegalStateException e) {
-                return Collections.emptyList();
-            }
-        }
-
+                Iterable<Jogador> jogadoresIterable = jogadorService.findAll();
+                List<Jogador> jogadoresList = StreamSupport.stream(jogadoresIterable.spliterator(), false)
+                    .collect(Collectors.toList());
+                return jogadoresList;
+    } catch (IllegalStateException e) {
+        return Collections.emptyList();
+    }
+}
+        
         @GetMapping("/{id}")
         public Jogador findAll(@PathVariable Integer id) throws ObjectNotFoundException {
-            Optional<Jogador> jogadorOptional = repository.findById(id);
-            if(jogadorOptional.isPresent()) {
+            Optional<Jogador> jogadorOptional = jogadorService.findById(id);
+            if (jogadorOptional.isPresent()) {
                 return jogadorOptional.get();
-            }else{
-                throw new ObjectNotFoundException(id, "Jogador não encontrado com o ID " + id);
-
-                }
-            }
-
-        
+    }       else {
+        throw new ObjectNotFoundException(id, "Jogador não encontrado com o ID " + id);
+    }
+}
         @PostMapping("/salvar")
-        public ResponseEntity<Jogador>salvar(@RequestBody Jogador jogador){
-            return ResponseEntity.ok(repository.save(jogador));
-        }
+        public ResponseEntity<Jogador> salvar(@RequestBody Jogador jogador) {
+            return ResponseEntity.ok(jogadorService.save(jogador));
+}
+        
         
         
         @GetMapping("/lol/summoner/v4/summoners/by-name/{summonerName}")
