9b0e386de5627389483e851916b6583e49051367
Dina Gomes <74661038+dinapgomes@users.noreply.github.com>
2023-07-06 00:29:24 +0000

    adcionando componente na tela de grupo

--- frontend/src/pages/app/Grupo/index.tsx
+++ frontend/src/pages/app/Grupo/index.tsx
@@ -1,7 +1,18 @@
+import CardLaneGroup from "../../../components/CardLaneGroup";
+
 const Grupo = () => {
     return(
         <div className="app-area">
             <h1>Grupo</h1>
+            <div className='flex justify-around mt-10'>   
+
+                <CardLaneGroup lane='topo' name='topo' />
+                <CardLaneGroup lane='selva' name='selva' />
+                <CardLaneGroup lane='meio' name='meio' />
+                <CardLaneGroup lane='atirador' name='atirador' />
+                <CardLaneGroup lane='suporte' name='suporte' />
+
+                </div>
         </div>
     );
 };
