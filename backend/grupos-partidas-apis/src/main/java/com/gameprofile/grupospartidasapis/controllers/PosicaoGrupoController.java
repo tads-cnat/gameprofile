cffa907a739fca502c58b1eb33d186c600b5138b
Ruan Franklin <94229859+Ruan-Franklin@users.noreply.github.com>
2023-07-09 00:06:41 +0000

    :bug: fix: ajusta tipo de dado passado

--- backend/grupos-partidas-apis/src/main/java/com/gameprofile/grupospartidasapis/controllers/PosicaoGrupoController.java
+++ backend/grupos-partidas-apis/src/main/java/com/gameprofile/grupospartidasapis/controllers/PosicaoGrupoController.java
@@ -4,10 +4,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import java.util.Optional;
 import java.util.List;
+import java.util.Map;
 import com.gameprofile.grupospartidasapis.entities.Grupo;
 import com.gameprofile.grupospartidasapis.entities.PosicaoGrupo;
 import com.gameprofile.grupospartidasapis.entities.Jogador;
@@ -26,51 +28,61 @@ public class PosicaoGrupoController {
         this.grupoRepository = grupoRepository;
         this.jogadorRepository = jogadorRepository;
     }
-    @PostMapping("/{grupoId}/jogador/{jogadorId}/{posicao}")
-    public ResponseEntity<String> entrarPosicaoGrupo( @PathVariable Integer grupoId,@PathVariable Integer jogadorId,@PathVariable String posicao){
+    
+    @PostMapping("/entrar")
+    public ResponseEntity<String> entrarPosicaoGrupo(@RequestBody Map<String, Object> requestBody) {
+        Integer grupoId = (Integer) requestBody.get("grupoId");
+        Integer jogadorId = (Integer) requestBody.get("jogadorId");
+        String posicao = (String) requestBody.get("posicao");
+    
         Optional<Grupo> grupoOptional = grupoRepository.findById(grupoId);
         Optional<Jogador> jogadorOptional = jogadorRepository.findById(jogadorId);
-
-        if (grupoOptional.isEmpty() | jogadorOptional.isEmpty()){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("JOgador não encontrado");
+    
+        if (grupoOptional.isEmpty() || jogadorOptional.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Jogador não encontrado");
         }
+    
         Grupo grupo = grupoOptional.get();
         Jogador jogador = jogadorOptional.get();
-        List <PosicaoGrupo> posicoesGrupo = posicaoGrupoRepository.findByGrupoAndStatus(grupo, false);
+        List<PosicaoGrupo> posicoesGrupo = posicaoGrupoRepository.findByGrupoAndStatus(grupo, false);
         PosicaoGrupo posicaoGrupo = posicoesGrupo.stream()
-            .filter(pg -> {
-                switch (posicao) {
-                    case "topo":
-                        return grupo.getTopo().equals(pg);
-                    case "selva":
-                        return grupo.getSelva().equals(pg);
-                    case "meio":
-                        return grupo.getMeio().equals(pg);
-                    case "atirador":
-                        return grupo.getAtirador().equals(pg);
-                    case "suporte":
-                        return grupo.getSuporte().equals(pg);
-                    default:
-                        return false;
-                }
-            })
-            .findFirst()
-            .orElse(null);
-
-        if (posicaoGrupo == null){
+                .filter(pg -> {
+                    switch (posicao) {
+                        case "topo":
+                            return grupo.getTopo().equals(pg);
+                        case "selva":
+                            return grupo.getSelva().equals(pg);
+                        case "meio":
+                            return grupo.getMeio().equals(pg);
+                        case "atirador":
+                            return grupo.getAtirador().equals(pg);
+                        case "suporte":
+                            return grupo.getSuporte().equals(pg);
+                        default:
+                            return false;
+                    }
+                })
+                .findFirst()
+                .orElse(null);
+    
+        if (posicaoGrupo == null) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Posição não encontrada no grupo!");
         }
-        if(posicaoGrupo.getJogador() != null){
+    
+        if (posicaoGrupo.getJogador() != null) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A posição já está ocupada!");
         }
-        if(!posicaoValida(grupo, posicao)){
+    
+        if (!posicaoValida(grupo, posicao)) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Posição inválida!");
         }
+    
         posicaoGrupo.setJogador(jogador);
         posicaoGrupo.setStatus(true);
         posicaoGrupoRepository.save(posicaoGrupo);
         return ResponseEntity.status(HttpStatus.OK).body("Jogador adicionado com sucesso!");
     }
+
     private boolean posicaoValida(Grupo grupo, String posicao){
         switch(posicao){
             case "topo":
